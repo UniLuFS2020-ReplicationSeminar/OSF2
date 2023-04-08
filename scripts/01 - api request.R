74fd8045ddb1d33fa7cef4777c1849a3fbfcba96
simonweigold <simonw750@gmail.com>
2023-04-08 17:34:34 +0200

    added code which uses the guardianapi package to request data.

---
+++ scripts/01 - api request.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(httr)
+library(guardianapi)
+library(here)
+
+# Using guardianapi package -----------------------------------------------------
+
+# Load API key
+gu_api_key()
+
+# GET request
+api_request <- gu_content(query = "amazon",
+                          from_date = "2000-01-01",
+                          to_date = "2022-12-31")
+
+# Save data as data frame
+df <- api_request %>% select(-tags)
+df <- as.data.frame(df)
+write.csv(df, here::here("guardian_amazon.csv"))
+
+
+
