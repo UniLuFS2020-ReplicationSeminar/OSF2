301af273f33e427f9c5f885e6439b0e4f2f4b069
simonweigold <simonw750@gmail.com>
2023-04-08 17:36:22 +0200

    added code section for an alternative way to request data.

--- scripts/01 - api request.R
+++ scripts/01 - api request.R
@@ -19,4 +19,22 @@ df <- as.data.frame(df)
 write.csv(df, here::here("guardian_amazon.csv"))
 
 
+# Manual GET request using httr package -----------------------------------
+
+# Set API key and endpoint URL
+api_key <- rstudioapi::askForPassword()
+endpoint <- "https://content.guardianapis.com/search"
+
+# Set query parameters
+parameters <- list(q = "amazon",
+                   from_date = "2000-01-01",
+                   to_date = "2022-12-31",
+                   page_size = 5000)
+
+# Send GET request to API and retrieve response
+response <- GET(endpoint, query = c(parameters, list(apiKey = api_key)))
+
+# Extract data from response
+data <- content(response, "parsed")
+articles <- data$response$results
 
