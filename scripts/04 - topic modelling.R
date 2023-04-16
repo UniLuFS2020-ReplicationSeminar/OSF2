61db695865a48e65c30a13fe1cd8d770d42261aa
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-04-16 22:38:51 +0200

    Create model from corpus, visualise results in bar plot

--- scripts/04 - topic modelling.R
+++ scripts/04 - topic modelling.R
@@ -5,21 +5,59 @@
 # Setup --------------------------------------------------------------
 library(quanteda)
 library(seededlda)
+library(ggplot2)
+library(tidyverse)
 
-
-# sample
-#sample <- sample_n(guardian_amazon_corpus, 100)
+load("guardian_amazon_corpus.RData")
 
 # Clean Data --------------------------------------------------------------
-sample_toks <- quanteda::tokens(sample$body_text, remove_punct = TRUE, remove_numbers = TRUE, remove_symbol = TRUE)
-sample_toks <- quanteda::tokens_remove(sample_toks, pattern = stopwords("en"))
+corpus_tokens <- quanteda::tokens(guardian_amazon_corpus$body_text, remove_punct = TRUE, remove_numbers = TRUE, remove_symbol = TRUE)
+corpus_tokens <- quanteda::tokens_remove(corpus_tokens, pattern = stopwords("en"))
 
-dfmat_news <- dfm(sample_toks) %>% 
+# Save and load corpus_tokens to file to save time
+# save(corpus_tokens, file = "corpus_tokens.RData")
+load("corpus_tokens.RData")
+
+dfm_corpus <- dfm(corpus_tokens) %>% 
   dfm_trim(min_termfreq = 0.8, termfreq_type = "quantile",
            max_docfreq = 0.1, docfreq_type = "prop")
 
+# Save and load corpus_tokens to file to save time
+#save(dfm_corpus, file = "dfm_corpus.RData")
+load("dfm_corpus.RData")
 
-tmod_lda <- textmodel_lda(dfmat_news, k = 2)
-terms(tmod_lda, 4)
+lda_corpus_k10 <- textmodel_lda(dfm_corpus, k = 10)
+lda_corpus_k4 <- textmodel_lda(dfm_corpus, k = 4)
 
+# Save and load corpus_tokens to file to save time
+#save(lda_corpus_k10, file = "lda_corpus_k10.RData")
+load("lda_corpus_k10.RData")
 
+terms(lda_corpus_k10, 10)
+terms_list <- terms(lda_corpus_k10, 10)
+
+# assign topic as a new document-level variable$
+dfm_corpus$topic_k10 <- topics(lda_corpus_k10)
+
+# cross-table of the topic frequency
+topic_frequency <- table(dfm_corpus$topic_k10)
+topic_frequency <- as.data.frame(topic_frequency)
+
+topic_frequency <- topic_frequency %>% 
+  mutate(Var1 = case_when(Var1 == "topic1" ~ "Publishing",
+                          Var1 == "topic2" ~ "Football",
+                          Var1 == "topic3" ~ "Environment",
+                          Var1 == "topic4" ~ "Travel",
+                          Var1 == "topic5" ~ "COVID-19",
+                          Var1 == "topic6" ~ "Economy",
+                          Var1 == "topic7" ~ "TV & Films",
+                          Var1 == "topic8" ~ "Politics",
+                          Var1 == "topic9" ~ "Digital",
+                          Var1 == "topic10" ~ "Amazon Inc."))
+
+ggplot(topic_frequency, aes(x = reorder(Var1, Freq), y = Freq)) +
+  geom_bar(stat="identity") + 
+  xlab(" ") +
+  ylab("Number of Articles") +
+  theme_minimal() +
+  coord_flip()
