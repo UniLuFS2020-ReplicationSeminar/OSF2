a4c3f6f1cbf4153bf8e6d09c88f274cad4f43a89
Oldscha <olga.shpakova@hotmail.com>
2023-04-16 21:04:10 +0200

    Add the frequency numbers to the labels

--- scripts/04 - frequency plot.R
+++ scripts/04 - frequency plot.R
@@ -13,11 +13,11 @@ load("guardian_amazon_corpus.RData")
 
 guardian_amazon_title <- guardian_amazon_corpus
 
-# create a corpus object 
+# create corpus object 
 
 guardian_amazon_title <- Corpus(VectorSource(guardian_amazon_title$web_title))
 
-# built a function to remove HTML tags from a text 
+# built function to remove HTML tags from a text 
 
 removeHTML <- function(text){
   text = gsub(pattern = '<. +\\">', '', text)
@@ -44,19 +44,20 @@ guardian_amazon_title <- guardian_amazon_title %>%
   tm_map(removeWords, stopwords("english")) %>%
   tm_map(removeWords, stopwords("SMART"))
 
-# create a TDM object with word frequencies
+# create TDM object with word frequencies
 
 tdm <- TermDocumentMatrix(guardian_amazon_title) %>%
   as.matrix()
 
-# create a vector of the total frequency of each word
+# create vector of the total frequency of each word
 
 words <- sort(rowSums(tdm), decreasing = TRUE)
 
-# create a dataframe with two columns (unique words from guardian_amazon_title corpus 
+# create dataframe with two columns (unique words from guardian_amazon_title corpus 
 # and corresponding frequency of each word)
 
 df <- data.frame(word = names(words), freq = words)
+df$word_freq <- paste(df$word, df$freq, sep = ": \n")
 
 # remove rows with strings lower as two characters
 
@@ -64,44 +65,28 @@ df <- df %>%
   filter(nchar(as.character(word)) >2,
          word !="don'")
 
-# create a data set with top 35 words
+# create data frame with top 35 words
 
 df_top35 <- head(df, 35)
 
 library("viridis")
 
-# create a radar plot with "most loud" 35 words
+# create a radar plot with top 35 words
 
-ggplot(data=df_top35, aes(x=word, y=freq, fill=word))+
-   geom_bar(width = 0.75, stat = "identity", colour = "black", size = 1)+
+ggplot(data=df_top35, aes(x=word_freq, y=freq, fill=word_freq))+
+   geom_bar(width = 0.75, stat = "identity", colour = "black", size = 0.3)+
   scale_fill_viridis(discrete = TRUE,
-                     option = "B")+
+                     option = "D")+
   coord_polar(theta = "x")+
   coord_polar(start = 0)+
     ylim(-2000, 2200)+
-  ggtitle("Top 35 Words by Frequency")+
+  ggtitle("Top 35 Words of Guardian Article Titles on Amazon by Frequency")+
   xlab("")+
   ylab("")+
   theme_minimal()+
   theme(legend.position = "none")+
   labs(x = NULL, y = NULL)
+ 
 
-# create a radar plot with "most salient" 35 words
-
-df_tail35 <- tail(df, 35)
-
-ggplot(data=df_tail35, aes(x=word, y=freq, fill=word))+
-  geom_bar(width = 0.75, stat = "identity", colour = "black", size = 1)+
-  scale_fill_viridis(discrete = TRUE,
-                     option = "B")+
-  coord_polar(theta = "x")+
-  coord_polar(start = 0)+
-  ylim(-2000, 2200)+
-  ggtitle("Bottom 35 Words by Frequency")+
-  xlab("")+
-  ylab("")+
-  theme_minimal()+
-  theme(legend.position = "none")+
-  labs(x = NULL, y = NULL)
 
 
