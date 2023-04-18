8255ced98bf8b1a10457f03eab38a851f38bd301
simonweigold <simonw750@gmail.com>
2023-04-18 21:10:34 +0200

    renamed "endpoint URL" to "base URL" and added a section which defines the search query URL.

--- scripts/01 - api request.R
+++ scripts/01 - api request.R
@@ -24,18 +24,26 @@ write.csv(df, here::here("guardian_amazon.csv"))
 
 # Manual GET request using httr package -----------------------------------
 
-# Set API key and endpoint URL
+# Set API key and base URL
 api_key <- rstudioapi::askForPassword()
-endpoint <- "https://content.guardianapis.com/search"
+base_url <- "https://content.guardianapis.com/search"
 
 # Set query parameters
-parameters <- list(q = "amazon",
-                   from_date = "2000-01-01",
-                   to_date = "2022-12-31",
-                   page_size = 5000)
+parameters <- list("amazon",
+                   "from-date" = "2000-01-01",
+                   "to-date" = "2022-12-31")
+
+# Define search query URL
+search_query_url <- paste0(
+  base_url, search_query, "api-key=", getOption("gu.API.key"),
+  fields_query, dots_query, show_tags_query, tag_query, from_date_q,
+  to_date_q
+)
+
+query_url <- str_c(endpoint, URLencode(parameters), "&api-key", api_key)
 
 # Send GET request to API and retrieve response
-response <- GET(endpoint, query = c(parameters, list(apiKey = api_key)))
+response <- httr::GET(query_url)
 
 # Extract data from response
 data <- content(response, "parsed")
