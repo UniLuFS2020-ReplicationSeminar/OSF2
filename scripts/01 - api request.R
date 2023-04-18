543435777ebc9609a9df9d375133fe1982067b50
simonweigold <simonw750@gmail.com>
2023-04-18 22:27:20 +0200

    fixed the search query url.

--- scripts/01 - api request.R
+++ scripts/01 - api request.R
@@ -34,16 +34,22 @@ parameters <- list("amazon",
                    "to-date" = "2022-12-31")
 
 # Define search query URL
-search_query_url <- paste0(
-  base_url, "?q=", utils::URLencode(parameters[[1]]), "api-key=", api_key,
-  "&show-fields=", "all",
-  "&show-tags=", "all",
-  "&from-date=", parameters[[2]],
-  "&to-date=", parameters[[3]]
-)
+search_query_url <- str_c("https://content.guardianapis.com/search?q=amazon&from-date=2000-01-01&to-date=2022-12-31&api-key=", api_key)
+#search_query_url <- paste0(
+  #base_url,
+  #"?q=", parameters[[1]],
+  #"&from-date=", "2020-04-01",
+  #"&to-date=", "2020-04-10",
+  #"?api-key=", api_key
+#)
 
 # Send GET request to API and retrieve response
 response <- httr::GET(search_query_url)
+
+#
+df <- jsonlite::fromJSON(paste0(search_query_url, "&page-size=50"),
+                         flatten = TRUE
+)
 
 # Extract data from response
 data <- content(response, "parsed")
